8964f4e415de0bef1c058a8a24c5c47fdec4c78f
DZoltan <55488265+DZoltan@users.noreply.github.com>
2024-02-25 18:59:56 +0100

    shop and detail View

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/termekek">Shop</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import DetailView from "@/views/DetailView.vue";
+import ShopView from "@/views/ShopView.vue";
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/termekek',
+    name: 'Shop',
+    component: ShopView
+  },
+  {
+    path: '/termek',
+    name: 'termek',
+    component: DetailView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
