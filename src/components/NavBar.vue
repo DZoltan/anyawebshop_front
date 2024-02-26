3a74c65cdd29f8a60c566bdb1737f5b0afb90608
DZoltan <55488265+DZoltan@users.noreply.github.com>
2024-02-26 18:49:04 +0100

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> | 
+    <router-link to="/about">About</router-link> |
     <router-link to="/termekek">Shop</router-link>
   </nav>
   <router-view/>
